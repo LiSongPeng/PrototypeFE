ecf0ab7f9046ad2617fc26d3f7c2069630ba684b
JChang07 <guaishou07@yeah.net>
2017-09-14 10:57:28 +0800

    添加主页功能页

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require ('express') ;
+var path = require('path');
+var app = express() ;
+// var router = require('./routes/index');
+
+app.use(express.static (path.join(__dirname, 'public')));
+app.set ('port', process.env.PORT || 14250) ;
+
+app.use('/',function (req,res,next) {
+    res.sendfile("./public/index.html");
+});
+module.exports = app ;
